f2fd00ce3d93d069ee9557c81ee6390942bbebb7
Vladislav Melnikov <40246526+whatisloveam@users.noreply.github.com>
2024-01-20 19:08:24 +0500

    Update background.py

--- background.py
+++ background.py
@@ -3,9 +3,3 @@ import helper.image as image
 from helper.settings import *
 
 class Background:
-    def __init__(self):
-        self.image = image.load("Assets/background.jpg", size=(SCREEN_WIDTH, SCREEN_HEIGHT),
-                                convert="default")
-
-    def draw(self, surface):
-        image.draw(surface, self.image, (SCREEN_WIDTH//2, SCREEN_HEIGHT//2), pos_mode="center")
